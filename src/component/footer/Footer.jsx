cde38eb12f9fa231e2b6b39b0bbf8186004576da
sumanmoktan <sumantamang662@gmail.com>
2025-06-06 11:54:21 +0545

    footer code push in github

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -1,9 +1,101 @@
-import React from 'react'
+import footerlog from "../../assets/footer-logo.png";
+import ScrollLink from "../links/ScrollLinks";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaTwitter,
+  FaMapMarkerAlt,
+  FaPhone,
+  FaEnvelopeOpenText,
+} from "react-icons/fa";
+import { links, footerGallery } from "../../Data";
+import "./footer.css";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer">
+      <div className="footer-container container grid">
+        <div>
+          <a href="/" className="footer-logo">
+            <img src={footerlog} alt="" className="footer-logo-img" />
+          </a>
+          <p className="footer-description">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos eius
+            maiores consequatur possimus.
+          </p>
+          <div className="footer-socials">
+            <a
+              href="https://www.facebook.com/"
+              target="_blank"
+              className="footer-social-link"
+            >
+              <FaFacebook />
+            </a>
+            <a
+              href="https://www.instagram.com/"
+              target="_blank"
+              className="footer-social-link"
+            >
+              <FaInstagram />
+            </a>
+            <a
+              href="https://www.x.com/"
+              target="_blank"
+              className="footer-social-link"
+            >
+              <FaTwitter />
+            </a>
+          </div>
+        </div>
+        <div>
+          <h3 className="footer-title">Quick Links</h3>
+          <ul className="footer-links grid">
+            {links.map((link, index) => {
+              return (
+                <li className="footer-item" key={index}>
+                  <ScrollLink to={link} name={link} className="footer-link" />
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div>
+          <h3 className="footer-title">Contact</h3>
+          <div className="footer-contact grid">
+            <div className="footer-item">
+              <FaMapMarkerAlt className="footer-icon" />
+              <p className="footer-info">
+                2055 Niagara falls blvd, Amherst Ny 1556
+              </p>
+            </div>
+            <div className="footer-item">
+              <FaPhone className="footer-icon" />
+              <p className="footer-info">07789531264</p>
+            </div>
+            <div className="footer-item">
+              <FaEnvelopeOpenText className="footer-icon" />
+              <p className="footer-info">info@gmail.com</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <h3 className="footer-title">Gallery</h3>
+          <div className="footer-gallery grid">
+            {footerGallery.map((gallary, index) => {
+              return (
+                <img
+                  src={gallary}
+                  alt="photo"
+                  key={index}
+                  className="footer-img"
+                />
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
