7dc0d95ea2129f72cc43096e7683f8a92273a93e
sumanmoktan <sumantamang662@gmail.com>
2025-06-12 08:56:43 +0545

    changes a content in code

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,62 +1,62 @@
-import category1 from './assets/category-1.png';
-import category2 from './assets/category-2.png';
-import category3 from './assets/category-3.png';
-import category4 from './assets/category-4.png';
-import category5 from './assets/category-5.png';
-import category6 from './assets/category-6.png';
+import category1 from "./assets/category-1.png";
+import category2 from "./assets/category-2.png";
+import category3 from "./assets/category-3.png";
+import category4 from "./assets/category-4.png";
+import category5 from "./assets/category-5.png";
+import category6 from "./assets/category-6.png";
 
-import destination1 from './assets/destination-1.png';
-import destination2 from './assets/destination-2.png';
-import destination3 from './assets/destination-3.png';
-import destination4 from './assets/destination-4.png';
-import destination5 from './assets/destination-5.png';
-import destination6 from './assets/destination-6.png';
+import destination1 from "./assets/destination-1.png";
+import destination2 from "./assets/destination-2.png";
+import destination3 from "./assets/destination-3.png";
+import destination4 from "./assets/destination-4.png";
+import destination5 from "./assets/destination-5.png";
+import destination6 from "./assets/destination-6.png";
 
-import tour1 from './assets/tour-1.png';
-import tour2 from './assets/tour-2.png';
-import tour3 from './assets/tour-3.png';
-import tour4 from './assets/tour-4.png';
-import tour5 from './assets/tour-5.png';
-import tour6 from './assets/tour-6.png';
+import tour1 from "./assets/tour-1.png";
+import tour2 from "./assets/tour-2.png";
+import tour3 from "./assets/tour-3.png";
+import tour4 from "./assets/tour-4.png";
+import tour5 from "./assets/tour-5.png";
+import tour6 from "./assets/tour-6.png";
 
-import gallery1 from './assets/gallery-1.png';
-import gallery2 from './assets/gallery-2.png';
-import gallery3 from './assets/gallery-3.png';
-import gallery4 from './assets/gallery-4.png';
-import gallery5 from './assets/gallery-5.png';
-import gallery6 from './assets/gallery-6.png';
+import gallery1 from "./assets/gallery-1.png";
+import gallery2 from "./assets/gallery-2.png";
+import gallery3 from "./assets/gallery-3.png";
+import gallery4 from "./assets/gallery-4.png";
+import gallery5 from "./assets/gallery-5.png";
+import gallery6 from "./assets/gallery-6.png";
 
-export const links = ['home', 'about', 'destinations', 'tours'];
+export const links = ["home", "about", "destinations", "tours"];
 
 export const categories = [
   {
     img: category1,
-    title: 'Beach',
+    title: "Trekking and Mounteering",
   },
 
   {
     img: category2,
-    title: 'Mountain',
+    title: "Cultural Heritage and Temples",
   },
 
   {
     img: category3,
-    title: 'Desert',
+    title: "Wildlife and Jungle Safaris",
   },
 
   {
     img: category4,
-    title: 'Temple',
+    title: "Adventure Sports",
   },
 
   {
     img: category5,
-    title: 'Cities',
+    title: "Lakes and Scenic Retreats",
   },
 
   {
     img: category6,
-    title: 'Pyramid',
+    title: "Rural and Community Tourism",
   },
 ];
 
@@ -64,7 +64,7 @@ export const destinations = [
   {
     id: 1,
     img: destination1,
-    title: 'California',
+    title: "Everest Base Camp",
     price: 349,
     rating: 3.5,
   },
@@ -72,7 +72,7 @@ export const destinations = [
   {
     id: 2,
     img: destination2,
-    title: 'Spain',
+    title: "Annapurna Base Camp",
     price: 249,
     rating: 3.5,
   },
@@ -80,7 +80,7 @@ export const destinations = [
   {
     id: 3,
     img: destination3,
-    title: 'Japan',
+    title: "Langtang Valley",
     price: 489,
     rating: 3.5,
   },
@@ -88,7 +88,7 @@ export const destinations = [
   {
     id: 4,
     img: destination4,
-    title: 'France',
+    title: "Manaslu Circuit",
     price: 389,
     rating: 3.5,
   },
@@ -96,7 +96,7 @@ export const destinations = [
   {
     id: 5,
     img: destination5,
-    title: 'England',
+    title: "Upper Mustang",
     price: 239,
     rating: 3.5,
   },
@@ -104,7 +104,7 @@ export const destinations = [
   {
     id: 6,
     img: destination6,
-    title: 'New York',
+    title: "Poon Hill",
     price: 449,
     rating: 3.5,
   },
@@ -114,9 +114,9 @@ export const tours = [
   {
     id: 1,
     img: tour1,
-    title: 'Eiffel Tower',
-    location: 'Paris, France',
-    days: '5 Days',
+    title: "Everest Base Camp",
+    location: "Paris, France",
+    days: "5 Days",
     price: 268,
     stars: 5,
   },
@@ -124,9 +124,9 @@ export const tours = [
   {
     id: 2,
     img: tour2,
-    title: 'Burj Khalifa',
-    location: 'Dubai, UAE',
-    days: '6 Days',
+    title: "Annapurna Base Camp",
+    location: "Dubai, UAE",
+    days: "6 Days",
     price: 367,
     stars: 4,
   },
@@ -134,9 +134,9 @@ export const tours = [
   {
     id: 3,
     img: tour3,
-    title: 'The Grand Canyon',
-    location: 'Arizona',
-    days: '8 Days',
+    title: "Manaslu",
+    location: "Arizona",
+    days: "8 Days",
     price: 201,
     stars: 3,
   },
@@ -144,9 +144,9 @@ export const tours = [
   {
     id: 4,
     img: tour4,
-    title: 'Mountain Hiking Tour',
-    location: 'Nevada, US',
-    days: '5 Days',
+    title: "Langtang",
+    location: "Nevada, US",
+    days: "5 Days",
     price: 201,
     stars: 4,
   },
@@ -154,9 +154,9 @@ export const tours = [
   {
     id: 5,
     img: tour5,
-    title: 'Desert Seeing',
-    location: 'Rajasthan, India',
-    days: '6 Days',
+    title: "Mardi Himal",
+    location: "Rajasthan, India",
+    days: "6 Days",
     price: 367,
     stars: 4,
   },
@@ -164,9 +164,9 @@ export const tours = [
   {
     id: 6,
     img: tour6,
-    title: 'Louvre Museum',
-    location: 'Paris, France',
-    days: '8 Days',
+    title: "Gokyo Lake",
+    location: "Paris, France",
+    days: "8 Days",
     price: 201,
     stars: 4,
   },
